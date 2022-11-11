99313e97f39083ec5e0bf42aaed96b9284b644ab
bendn <bend.n@outlook.com>
2022-11-11 19:29:32 +0700

    better motors

--- car_driver/arduino/drive.h
+++ car_driver/arduino/drive.h
@@ -11,8 +11,38 @@ inline void begin() {
   motor_b.begin();
 }
 
-inline void stop() {
+inline void brake() {
   motor_a.brake();
   motor_b.brake();
+}
+
+inline void forward(uint8_t speed) {
+  motor_a.forward(speed);
+  motor_b.forward(speed);
+}
+
+inline void forward(uint8_t a_speed, uint8_t b_speed) {
+  motor_a.forward(a_speed);
+  motor_b.forward(b_speed);
+}
+
+inline void backward(uint8_t speed) {
+  motor_a.backward(speed);
+  motor_b.backward(speed);
+}
+
+inline void backward(uint8_t a_speed, uint8_t b_speed) {
+  motor_a.backward(a_speed);
+  motor_b.backward(b_speed);
+}
+
+inline void speed(int8_t speed) {
+  motor_a.speed(speed);
+  motor_b.speed(speed);
+}
+
+inline void speed(uint8_t a_speed, uint8_t b_speed) {
+  motor_a.speed(a_speed);
+  motor_b.speed(b_speed);
 }
 #endif
--- car_driver/arduino/motor.h
+++ car_driver/arduino/motor.h
@@ -1,22 +1,40 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-#include "motor_utils.h"
-
-struct Motor {
+class Motor {
+private:
   int pin;
   int brake_pin;
   int speed_pin;
 
-  inline void forward(int8_t speed) {
-    motor_utils::set(pin, brake_pin, speed_pin, HIGH, speed);
+  // @param direction LOW is backwards, HIGH is forward
+  // @param speed 0-255
+  void set(int8_t direction, uint8_t speed) {
+    digitalWrite(pin, direction);
+    digitalWrite(brake_pin, LOW);  // brake off
+    analogWrite(speed_pin, speed); // speed up
   }
-  inline void backward(int8_t speed) {
-    motor_utils::set(pin, brake_pin, speed_pin, LOW, speed);
-  }
-  inline void brake() { motor_utils::brake(brake_pin, speed_pin); }
 
-  inline void begin() {
+public:
+  // @param speed -100-100, the speed to go at, negative = backwards
+  void speed(int8_t speed) {
+    if (speed == 0)
+      brake();
+    else if (speed < 0)
+      backward(abs(speed) + 155); // negative number: -100 + 355 = 255
+    else
+      forward(speed + 155); // positive number: 100 + 155 = 255
+  }
+
+  void brake() {
+    digitalWrite(brake_pin, HIGH); // engage brake
+    analogWrite(speed_pin, 0);     // slow
+  }
+
+  inline void forward(uint8_t speed) { set(HIGH, speed); }
+  inline void backward(uint8_t speed) { set(LOW, speed); }
+
+  void begin() {
     pinMode(pin, OUTPUT);
     pinMode(brake_pin, OUTPUT);
   }
